13a99c99a2a89df0609a83f25705f8d9a95f9136
JAESKAAA <jaeskaaa@gmail.com>
2022-01-07 19:15:52 +0900

    정렬, offset, 간단한 통계함수, join 관련 Querydsl 테스트코드

--- src/test/java/study/querydslprac/QuerydslBasicTest.java
+++ src/test/java/study/querydslprac/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydslprac;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,15 +10,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import study.querydslprac.entity.Member;
 import study.querydslprac.entity.QMember;
+import study.querydslprac.entity.QTeam;
 import study.querydslprac.entity.Team;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.transaction.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydslprac.entity.QMember.*;
+import static study.querydslprac.entity.QTeam.*;
 
 @SpringBootTest
 @Transactional
@@ -109,6 +115,233 @@ public class QuerydslBasicTest {
         long total = queryFactory
                 .selectFrom(member)
                 .fetchCount();
+    }
 
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순
+     * 2. 이름 올림 차순
+     *
+     * 단, 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort(){
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        //유저네임이 오름차순에 널값을 마지막으로 받도록 했으니 member5 -> member6 -> null순으로 예상함
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+    @Test
+    public void paging1(){
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1) //앞에 몇개를 스킵할건지 (0부터시작_ 즉, 여기서는 2번째 값부터 2개를 꺼내오라는 의미)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+    @Test
+    public void paging2(){
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1) //앞에 몇개를 스킵할건지 (0부터시작_ 즉, 여기서는 2번째 값부터 2개를 꺼내오라는 의미)
+                .limit(2)
+                .fetchResults();
+
+        
+        //쿼리결과의 토탈 갯수가 4개인지? -> beforeEach에서 값을 4개만 넣어줬으니 4가 나와야함
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        //쿼리결과에서 결과값의 사이즈가 2인지?
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void aggregation(){
+        List<Tuple> result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구하라
+     *
+     * select
+     */
+
+    @Test
+    public void group(){
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    /**
+     * 팀 A에 소속된 모든 회원 찾기
+     */
+    @Test
+    public void join1(){
+        List<Member> fetch = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(fetch)
+                .extracting("username")
+                .containsExactly("member1","member2");
+    }
+
+    //연관관계 없는 필드값으로 조인
+
+    /**
+     * 세타조인
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team) //member와 team테이블을 다 가져와서 조인시켜버림
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        result.forEach(System.out::println);
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA","teamB");
+    }
+
+    /**
+     * ex. 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     *
+     * JPQL = select m,t from Member m left join m.team t on t.name = 'teamA';
+     *
+     */
+    @Test
+    public void join_on_filtering(){
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        result.forEach(System.out::println);
+    }
+
+    /**
+     * 연관 관계가 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void join_on_no_relation(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+
+        //기존 세타조인은 left조인이 안되서 하기와 같이 on 활용하여 조인함
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                //원래같은경우 leftJoin(member.team, team)으로 조인함 이러면 join on절에 조인 대상이 id값으로 매칭되게 됨
+                //하지만 하기와 같이 작성하면 id값으로 매칭되지 않고 team.name으로만 매칭이 되게됨
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        result.forEach(System.out::println);
+    }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    public void fetchJoinNo(){
+        em.flush();
+        em.clear();
+
+        Member member1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        System.out.println(member1);
+
+        //member1에 team이 로딩됐는지 안됐는지 알려주는 로직
+        //현재 Member에 Team은 LAZY 로딩 상태이므로 false가 나와야 정상임
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(member1.getTeam());
+
+        assertThat(loaded).as("패치 조인 미적용").isFalse();
+    }
+
+    @Test
+    public void fetchJoinUse(){
+        em.flush();
+        em.clear();
+
+        Member member1 = queryFactory
+                .selectFrom(member)
+                //하기와 같이 fetchJoin()을 넣어주면 member를 조회할때 연관된 테이블정보를 한쿼리로 가져옴
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        System.out.println(member1);
+
+        //fetch조인이 적용되었기 때문에 true가 나와야 정상임
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(member1.getTeam());
+
+        assertThat(loaded).as("패치 조인 적용").isTrue();
     }
 }
